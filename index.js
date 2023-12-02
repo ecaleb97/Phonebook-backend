f98e7da7872dfd7d9234a63c3265458daa257640
ecaleb97 <valley_rob.0c@icloud.com>
2023-12-02 18:09:04 +0100

    Add .env file to gitignore

--- index.js
+++ index.js
@@ -1,120 +1,79 @@
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
+/* const morgan = require('morgan') */
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(cors())
 app.disable('x-powered-by')
 app.use(express.static('dist'))
 
-const PORT = process.env.PORT || 3001
-
-let persons = [
-  { 
-    id: 1,
-    name: "Arto Hellas", 
-    number: "040-123456"
-  },
-  { 
-    id: 2,
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
-  },
-  { 
-    id: 3,
-    name: "Dan Abramov", 
-    number: "12-43-234345"
-  },
-  { 
-    id: 4,
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
-  }
-]
-
-/* function requestLogger(request, response, next) {
-  console.log('Method:', request.method)
-  console.log('Path:', request.path)
-  console.log('Body:', request.body)
-  console.log('---')
-  next()
-} */
-
-morgan.token('body', (request, response) => JSON.stringify(request.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+/* morgan.token('body', (request, response) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')) */
 
 
 /* app.use(requestLogger)  */
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
+  const id = request.params.id
 
-  const person = persons.find(person => person.id === id)
-
-  if(person) {
+  Person.findById(id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
-app.get('/info', (request, response) => {
+/* app.get('/info', (request, response) => {
   const date = new Date()
 
   response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${date.toISOString().split('T')[0]}</p>
   `)
-})
+}) */
 
-function generateId() {
-  const ids = persons.map(person => person.id)
-  const maxId = persons.length > 0 
-    ? Math.max(...ids)
-    : 0
+app.post('/api/persons', async (request, response) => {
+  const body = request.body
 
-  return maxId + 1
-}
-
-app.post('/api/persons', (request, response) => {
-  const person = request.body
-
-  if(!person.name || !person.number) {
+  if(!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number is missing'
     })
   }
 
-  if(persons.find(p => p.name === person.name)) {
+  const names = await Person.find({})
+
+  const namesArray = names.map(name => name.name)
+
+  if(namesArray.includes(body.name)) {
     return response.status(400).json({
       error: 'name must be unique'
     })
   }
 
-  const newPerson = {
-    id: generateId(),
-    name: person.name,
-    number: person.number
-  }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-  persons = [...persons, newPerson]
-
-  /* console.log(persons) */
-
-  response.json(newPerson)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
+  const id = request.params.id
 
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(id).then(result => {
+    response.status(204).end()
+  })
 })
 
 function unknownEndpoint(request, response) {
@@ -122,6 +81,8 @@ function unknownEndpoint(request, response) {
 }
 
 app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
